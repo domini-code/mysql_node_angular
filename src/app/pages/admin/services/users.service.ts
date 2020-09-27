5257f76591b9bb721d4716c7abc6e70cc2542865
Bezael <bezel@gmail.com>
2020-09-27 10:33:39 +0200

    Add Crud

--- src/app/pages/admin/services/users.service.ts
+++ src/app/pages/admin/services/users.service.ts
@@ -16,38 +16,35 @@ export class UsersService {
       .get<User[]>(`${environment.API_URL}/users`)
       .pipe(catchError(this.handlerError));
   }
-  // http://localhost:3000/users
 
-  getById(userId: number): Observable<any> {
+  getById(userId: number): Observable<User> {
     return this.http
       .get<any>(`${environment.API_URL}/users/${userId}`)
       .pipe(catchError(this.handlerError));
   }
 
-  new(user: any): Observable<any> {
+  new(user: User): Observable<User> {
     return this.http
-      .post<any>(`${environment.API_URL}/users`, user)
+      .post<User>(`${environment.API_URL}/users`, user)
       .pipe(catchError(this.handlerError));
   }
 
-  update(userId: number, user: any): Observable<any> {
+  update(userId: number, user: User): Observable<User> {
     return this.http
-      .patch<any>(`${environment.API_URL}/users/${userId}`, user)
+      .patch<User>(`${environment.API_URL}/users/${userId}`, user)
       .pipe(catchError(this.handlerError));
   }
 
-  delete(userId: number): Observable<any> {
+  delete(userId: number): Observable<{}> {
     return this.http
-      .delete<any>(`${environment.API_URL}/users/${userId}`)
+      .delete<User>(`${environment.API_URL}/users/${userId}`)
       .pipe(catchError(this.handlerError));
   }
 
-  handlerError(error: any): Observable<never> {
-    let errorMessage = '';
-    if (error.error instanceof ErrorEvent) {
-      errorMessage = `Error  ${error.error.message}`;
-    } else {
-      errorMessage = `Error code: ${error.status} \n Message: ${error.message}`;
+  handlerError(error): Observable<never> {
+    let errorMessage = 'Error unknown';
+    if (error) {
+      errorMessage = `Error ${error.message}`;
     }
     window.alert(errorMessage);
     return throwError(errorMessage);
